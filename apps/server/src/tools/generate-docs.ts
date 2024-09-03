aa873a2505bafa2bd89052650366c398be6d1fb7
hata0 <haataa0100@gmail.com>
2024-09-03 16:50:01 +0900

    feat: generate-docs を作成

---
+++ apps/server/src/tools/generate-docs.ts
@@ -0,0 +1,21 @@
+import yaml from "yaml";
+import { writeFileSync } from "fs";
+import { app } from "../main";
+import { join } from "path";
+
+console.log("generating openapi docs");
+
+const docs = app.getOpenAPIDocument({
+  openapi: "3.0.0",
+  info: { version: "1.0.0", title: "Tutorial API" },
+});
+
+const fileContent = yaml.stringify(docs);
+
+const path = join(__dirname, "../generated/openapi-docs.yml");
+
+writeFileSync(path, fileContent, {
+  encoding: "utf-8",
+});
+
+console.log("success");
