2383b6ac17c9e2b96db352bac3c141f7d5d39907
hata0 <haataa0100@gmail.com>
2024-09-03 15:51:20 +0900

    feat: server を作成

---
+++ apps/server/src/main.ts
@@ -0,0 +1,16 @@
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono } from "@hono/zod-openapi";
+
+const app = new OpenAPIHono();
+
+app.doc("/doc", {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "Tutorial API",
+  },
+});
+
+app.get("/ui", swaggerUI({ url: "/doc" }));
+
+export { app };
